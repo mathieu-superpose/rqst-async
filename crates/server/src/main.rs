6ff5b42aaf9e286ce458b36f1f3fc68094b6a283
Mathieu Joly <m.joly@live.fr>
2024-12-20 23:17:37 +0100

    Closes #1 - non async fixed response from route /chat && POST method

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,10 +1,39 @@
-use miniserve::{Content, Request, Response};
+use miniserve::{http, Content, Request, Response};
 
 fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
 }
 
+fn chat(req: Request) -> Response {
+    match req {
+        Request::Post(body) => {
+            // parse the body as JSON
+            let parsed: Result<serde_json::Value, _> = serde_json::from_str(&body);
+            if parsed.is_err() {
+                return Err(http::StatusCode::BAD_REQUEST);
+            }
+            // extract the messages from the JSON
+            let parsed = parsed.unwrap();
+
+            let messages = parsed["messages"].as_array().cloned();
+
+            match messages {
+                Some(mut messages) => {
+                    messages.push("And how does that make you feel?".into());
+                    let json = serde_json::json!({ "messages": messages });
+                    Ok(Content::Json(json.to_string()))
+                }
+                None => Err(http::StatusCode::BAD_REQUEST),
+            }
+        }
+        _ => Err(http::StatusCode::METHOD_NOT_ALLOWED),
+    }
+}
+
 fn main() {
-    miniserve::Server::new().route("/", index).run()
+    miniserve::Server::new()
+        .route("/", index)
+        .route("/chat", chat)
+        .run()
 }
